cecb289a55e5ca1fe4454beef04f4a02d618c5db
Valgerður Kristinsdóttir <valgerdurk@gmail.com>
2021-04-19 23:42:07 +0000

    front page styling

--- src/components/film/Film.tsx
+++ src/components/film/Film.tsx
@@ -13,16 +13,22 @@ export function Film({ film  }: Props): JSX.Element {
       <h2 className={s.film__title}>
         {`Episode ${film.episodeID}: ${film.title}`}
       </h2>
-      <p className={s.film__text}>
-        {`${film.openingCrawl}`}
-      </p>
-      <h3>Characters</h3>
-      <div className={s.film__characters}>
-        {film.characterConnection.characters.map((character, i) => (
-          <Link key={i} href={`/characters/${character.id}`}>
-            {character.name}
-          </Link>
-        ))}
+      <div className={s.film__information}>
+        <pre className={s.film__text}>
+          {`${film.openingCrawl}`}
+        </pre>
+        <div className={s.film__characters}>
+          <h3>Characters</h3>
+          <div className={s.film__names}>
+            {film.characterConnection.characters.map((character, i) => (
+              <div className={s.film__name}>
+                <Link key={i} href={`/characters/${character.id}`}>
+                  {character.name}
+                </Link> 
+              </div> 
+            ))}
+          </div>
+        </div>
       </div>
     </section>
   );
--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -9,8 +9,12 @@ type Props = {
 export function Layout({ children }: Props): JSX.Element {
   return (
     <div className={s.layout}>
-      <Link href="/">Films</Link>
-      <Link href="/characters">Characters</Link>
+      <div className={s.layout__header}>
+        <ul className={s.layout__list}>
+          <li className={s.layout__listItem}><Link href="/">Films</Link></li>
+          <li className={s.layout__listItem}><Link href="/characters">Characters</Link></li>
+        </ul>
+      </div>
       <main>{children}</main>
     </div>
   );
